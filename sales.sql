3ad7a1ca01301a9a08612128d3d122daa68243b9
ElenaMakarova <ev.makarova84@gmail.com>
2016-04-19 23:14:48 +0400

    Update sales.sql

--- sales.sql
+++ sales.sql
@@ -1,5 +1,5 @@
 create table Sales (SalesID GUID not null, ProductID int not null, [Datetime] smalldatetime, CustomerID int)
 
 select s.ProductID, COUNT(*) as CountOfFirstPosition from Sales s inner join 
- (select SalesID, MAX([Datetime]) as FirstProductOfSales from Sales) fs on (s.SalesID=fs.SalesID and s.[Datetime]=fs.FirstProductOfSales)
+ (select sa.SalesID as SalID, MAX(sa.[Datetime]) as FirstProductOfSales from Sales sa) fs on (s.SalesID = fs.SalID and s.[Datetime] = fs.FirstProductOfSales)
 group by s.ProductID
